350bce3c868ad90de8b5c964a89a869513ecc9d1
ramsainixtec <ramsaini@youremail.com>
2026-04-09 08:09:36 +0530

    vercel setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,23 @@
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  import { defineConfig } from 'vite'
+  import react from '@vitejs/plugin-react'
   
+  export default defineConfig({
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
+      chunkSizeWarningLimit: 1600,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor'
+            }
+          }
+        }
+      }
+    }
+  })
+  createRoot(document.getElementById("root")!).render(<App />);
