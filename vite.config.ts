01407537343da563f721bce316818121cea7053a
ramsainixtec <ramsaini@youremail.com>
2026-04-09 08:32:32 +0530

    solved vercel error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [
@@ -14,7 +10,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve('./src'),  // ✅ __dirname ki zaroorat nahi
     },
   },
   assetsInclude: ['**/*.svg', '**/*.csv'],
